682140e8789c3c4b24b7505c92daa0ae2b18f13e
Puria Nafisi Azizi <puria@dyne.org>
2022-01-05 09:23:10 +0100

    Bump version

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # DYNETH configuration
 
-VERSION := 0.3
-DOCKER := dyne/dyneth:${VERSION}
+VERSION := 0.4
+DOCKER := ghcr.io/dyne/dyneth:${VERSION}
 NETWORK_ID := 1146703429
 P2P_PORT := 30303
 API_PORT := 8545
