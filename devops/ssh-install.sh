919f3986d11d943474fa7157aa1ee75f13341f58
Jaromil <jaromil@dyne.org>
2022-01-05 10:27:18 +0100

    small update to ssh provisioning script

--- devops/ssh-install.sh
+++ devops/ssh-install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+#
+# simple provisioning script for Debian/Ubuntu remote host
 
 H="$1"
 [[ "$H" == "" ]] && {
@@ -12,7 +14,20 @@ ssh $H 'printf "#!/bin/bash\nDEBIAN_FRONTEND="noninteractive" apt-get -q -y \$*\
 ssh $H bash ./apt.sh update
 
 echo "Installing Docker"
-scp ./scripts/docker-install.sh $H:
+tmp=`mktemp`
+cat <<EOF > $tmp
+echo "Installing Docker"
+bash ./apt.sh install ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/debian/gpg \
+    | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" \
+    | tee /etc/apt/sources.list.d/docker.list
+bash apt.sh update
+bash apt.sh install docker-ce docker-ce-cli containerd.io
+EOF
+scp $tmp $H:docker-install.sh
+rm -f $tmp
 ssh $H bash docker-install.sh
 
 echo "Installing Zenroom"
