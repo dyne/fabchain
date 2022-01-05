75427cd59cd37f161ae2bcc9f6a1b889253776b7
Pierluigi Maori <newmark@mangrovia.solutions>
2022-01-05 12:47:12 +0100

    fixed create account, now doesn't require zenroom, but python, added checks for zenroom and python binary, raised the gas limit in genesis

--- scripts/account.sh
+++ scripts/account.sh
@@ -2,6 +2,9 @@
 
 R=${DATA}
 
+OUTTMP=/tmp/dyneth_run
+PYTHON=python3
+
 function empty() {
     if [[ -r ${R}/keystore ]]; then
 	echo "ERROR: account already created, will not overwrite"
@@ -26,38 +29,68 @@ function geth() {
     docker run -it \
 	   --mount type=bind,source=${R},destination=/var/lib/dyneth \
 	   dyne/dyneth \
-	   setuidgid dyneth geth $* \
-	   --datadir /var/lib/dyneth \
-	   --keystore /var/lib/dyneth/keystore
+	   geth $* --datadir /var/lib/dyneth --keystore /var/lib/dyneth/keystore/tmp 
 }    
 function pk() {
-    conf=${1:-`find ${R}/keystore/ -type f`}
-    echo "print(JSON.decode(DATA).address)" | zenroom -a $conf 2> /dev/null
+    conf=${1:-`find ${R}/keystore/tmp -type f`}
+    cat $conf |$PYTHON -c "import json,sys;obj=json.load(sys.stdin);print(obj['address']);" 2>/dev/null
 }
 
+function check_python() {
+    if ! [ -x "$(command -v $PYTHON)" ]; then
+        echo 'Error: $PYTHON is not installed.' >&2
+        exit 1
+    fi
+}
+function check_zenroom() {
+    if ! [ -x "$(command -v zenroom)" ]; then
+        echo 'Error: zenroom is not installed.' >&2
+        exit 1
+    fi
+}
+function check_docker() {
+    if ! [ -x "$(command -v docker)" ]; then
+        echo 'Error: docker is not installed.' >&2
+        exit 1
+    fi
+}
 # main()
 case "$1" in
     new) empty
-	 geth account new
-	 conf=`find ${R}/keystore/ -type f`
-	 addr=`pk $conf`
-	 mv $conf ${R}/keystore/$addr
-	 ;;
+	 	check_python
+        check_docker
+	 	geth account new 
+	 	conf=`find ${R}/keystore/tmp -type f`
+	 	addr=`pk $conf`
+		if [ ! -f "$conf" ]; then
+			echo "cannot find key file" 2>/dev/null
+			exit 1
+		fi
+		if [ -z "$addr" ]; then
+			echo "cannot find addr key " 2>/dev/null
+			exit 1
+		fi
+		mv $conf ${R}/keystore/$addr
+		rm -rf ${R}/keystore/tmp/
+    echo "wallet saved in ${R}/keystore/$addr"
+	 	;;
 
     address) have
-	     conf=`find ${R}/keystore/ -type f`
-	     cat <<EOF | zenroom -a $conf 2> /dev/null
+	    check_zenroom
+	    conf=`find ${R}/keystore/ -type f`
+	    cat <<EOF | zenroom -a $conf 2> /dev/null
 conf=JSON.decode(DATA)
 print('PUBLIC ADDRESS:')
 print(' 0x'..conf.address)
 print('Genesis extradata:')
 print('0x'..O.zero(32):hex()..conf.address..O.zero(65):hex())
 EOF
-	     ;;
+	    ;;
 
     mine) have
-	  geth --unlock `pk` --mine
-	  ;;
+        check_docker
+	  	geth --unlock `pk` --mine
+		;;
 
     backup) have
 	    cat ${R}/keystore/*
@@ -65,16 +98,17 @@ EOF
 	    ;;
 
     restore) empty
-	     echo "TYPE YOUR SECRET KEY:"
-	     read basesk
-	     tmp=`mktemp`
-	     echo "$basesk" > $tmp
-	     addr=`pk $tmp`
-	     mkdir -p ${R}/keystore/
-	     echo $basesk > ${R}/keystore/${addr}
-	     echo "KEY RESTORED: $addr"
-	     ls -l ${R}/keystore/${addr}
-	     rm -f $tmp
-	     echo
+        check_python
+	    echo "TYPE YOUR SECRET KEY:"
+	    read basesk
+	    tmp=`mktemp`
+	    echo "$basesk" > $tmp
+	    addr=`pk $tmp`
+	    mkdir -p ${R}/keystore/
+	    echo $basesk > ${R}/keystore/${addr}
+	    echo "KEY RESTORED: $addr"
+	    ls -l ${R}/keystore/${addr}
+	    rm -f $tmp
+	    echo
 	    ;;	    
 esac
