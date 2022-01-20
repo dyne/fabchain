bc5bba1fb7f7893ab2e17006673727d90e62b6d6
Alberto Lerda <albertolerda97@gmail.com>
2022-01-20 08:32:43 +0100

    Shared code to ask for password

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,17 @@
+. scripts/secret-lib.sh
+
 contract="Storage"
 docker exec -it ${container} sh -c "cd /contracts && solc --overwrite --bin --abi \"${contract}.sol\" -o build"
-exit
+
+# ask for the password and save the secret key
+#sk=$(echo $(secret_key | tee /dev/tty) | tail -n 1 | awk '{print $NF}')
+secret_key
+res=$?
+if [ ! $res = 0 ]; then
+    echo && echo "Error: wrong password?" && echo
+    exit 1
+fi
+
 tmp=`mktemp`
 cat <<EOF >$tmp
 from web3 import Web3, HTTPProvider
@@ -18,7 +29,7 @@ if not abi or not bin:
   print("Error while reading abi or bin")
   exit(-1)
 
-sk = "..."
+sk = "$sk"
 
 w3 = Web3(HTTPProvider('http://localhost:8545'))
 account = w3.eth.account.privateKeyToAccount(sk)
@@ -37,3 +48,6 @@ print(w3.eth.sendRawTransaction(signed.rawTransaction).hex())
 EOF
 
 python $tmp
+
+
+rm -rf $tmp
