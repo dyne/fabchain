5e90c75d7ea650e4498ace4847bbe0ba152e117d
Jaromil <jaromil@dyne.org>
2022-02-04 14:34:17 +0100

    docker build use external images for geth and solc
    also pass all versions from config.mk

--- config.mk
+++ config.mk
@@ -7,6 +7,9 @@ P2P_PORT := 30303
 API_PORT := 8545
 DATA := $(shell pwd)/data
 CONTRACTS := $(shell pwd)/contracts
+GETH_VERSION := 1.10.14
+SOLC_VERSION := 0.8.11
+ALPINE_VERSION := 3.15
 
 # automatic
 UID = $(id -u)
