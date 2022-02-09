e65a15664c23b6e5b00e7618925f9f4598a39399
Jaromil <jaromil@dyne.org>
2022-02-10 00:21:04 +0100

    refinement of the whole devops journey

--- devops/join_enr.sh
+++ devops/join_enr.sh
@@ -1,8 +1,8 @@
 BN_FILE="bootnodes.csv"
 rm -f $BN_FILE 
-
+touch $BN_FILE
 for f in enr-*; do
   ip=`echo $f | cut -d'-' -f2`
   enr=`cat $f | xargs`
-  echo "$ip $enr" | tee -a $BN_FILE >/dev/null
+  echo "$ip $enr" >> $BN_FILE
 done
