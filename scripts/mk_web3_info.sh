34578276582c1518df4eaee3743e21b80d5c1ae8
Alberto Lerda <albertolerda97@gmail.com>
2022-02-17 15:48:44 +0100

    contract-info accepts nested dictionaries (like with logs in tx)

--- scripts/mk_web3_info.sh
+++ scripts/mk_web3_info.sh
@@ -2,13 +2,32 @@ TXID=$1
 cat <<EOF
 from web3 import Web3, HTTPProvider
 import json
-from hexbytes import HexBytes
 
 w3 = Web3(HTTPProvider('http://127.0.0.1:8545'))
 
+def toDict(dictToParse):
+  # convert any 'AttributeDict' type found to 'dict'
+  parsedDict = dict(dictToParse)
+  for key, val in parsedDict.items():
+    if 'list' in str(type(val)):
+      parsedDict[key] = [_parseValue(x) for x in val]
+    else:
+      parsedDict[key] = _parseValue(val)
+  return parsedDict
+
+def _parseValue(val):
+  # check for nested dict structures to iterate through
+  if 'dict' in str(type(val)).lower():
+    return toDict(val)
+  # convert 'HexBytes' type to 'str'
+  elif 'HexBytes' in str(type(val)):
+    return val.hex()
+  else:
+    return val
+
 try:
-  tx = dict(w3.eth.getTransactionReceipt("$TXID"))
-  tx = { k: v if type(v) != HexBytes else v.hex() for k, v in tx.items() }
+  tx = w3.eth.getTransactionReceipt("$TXID")
+  tx = toDict(tx)
   tx = json.dumps(tx, indent=2)
   print("Transaction recipe")
   print(tx)
